99d474840c330f57a92a799945e5b6de9c24a996
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-10-24 15:50:17 -0300

    created free_stack function and stack_define

--- main.c
+++ main.c
@@ -3,27 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nicolas <nicolas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 19:38:39 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/10/23 20:06:31 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/10/24 15:46:49 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stacks.h"
 
-int main(void)
+static	void free_stacks(t_stack	*stck_a, t_stack *stck_b)
+{
+	ft_clear_dlist(stck_a->top);
+	ft_clear_dlist(stck_b->top);
+	free(stck_a);
+	free(stck_b);
+}
+
+int main(int argc, char *argv[])
 {
 	t_stack *stck_a = ft_calloc(1, sizeof(t_stack));
 	t_stack *stck_b = ft_calloc(1, sizeof(t_stack));
-	int	i = -1;
 	
 	stck_b->top = NULL;
 	stck_b->btm = NULL;
 	stck_a->top = NULL;
 	stck_a->btm = NULL;
-	while (++i < 5)
-		ft_dladdbtm(&stck_a,ft_dlnew(i));
+	
+	if (!stack_define(argc, argv, stck_a))
+	{
+		free_stacks(stck_a, stck_b);
+		ft_putstr_fd("Error!\n", 2);
+		exit(1);
+	}
 	ft_printf("stack_a:\n");
 	print_stack(stck_a);
 	// printf("%d", stck_a->btm->value);
@@ -37,9 +49,5 @@ int main(void)
 	// stack_rotate(stck_a);
 	// ft_printf("stack_a:\n");
 	// print_stack(stck_a);
-	ft_clear_dlist(stck_a->top);
-	ft_clear_dlist(stck_b->top);
-	free(stck_a);
-	free(stck_b);
 	return (0);
 }
