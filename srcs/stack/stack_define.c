a277c0742c0254ab453ea5cb5861f7ff92d2c0da
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-10-24 19:36:12 -0300

    isn't digit validate added

--- srcs/stack/stack_define.c
+++ srcs/stack/stack_define.c
@@ -6,7 +6,7 @@
 /*   By: nicolas <nicolas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 18:39:47 by nicolas           #+#    #+#             */
-/*   Updated: 2023/10/24 15:43:27 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/10/24 17:01:45 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int stack_define(int argc, char **argv, t_stack *stack)
 	// fazer a tratativa caso uma string seja passada ou haja alguma letra entre os numeros
 	int		i;
 	int		j;
-
+	stack->top = NULL;
+	stack->btm = NULL;
 	if (argc < 2)
 		return (0);
 	i = 0;
