c9345b5a21b0480b131d6c86db5bc473547d725f
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-10-24 19:37:27 -0300

    print ss added

--- srcs/stack/stack_double_swap.c
+++ srcs/stack/stack_double_swap.c
@@ -3,21 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   stack_double_swap.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nicolas <nicolas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 18:29:34 by nicolas           #+#    #+#             */
-/*   Updated: 2023/10/13 19:21:14 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/10/24 19:37:07 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stacks.h>
-#include <libft.h>
+#include "stacks.h"
 
 int	stack_double_swap(t_stack	*stack_a, t_stack *stack_b)
 {
 	if (!stack_a->top || !stack_b->top)
 	{
-		ft_putstr_fd("Error: stack_a or stack_b is undefined!", 2);
+		ft_putstr_fd("Error!\n", 2);
 		return (0);
 	}
 	else if (stack_a->top && stack_b->top)
@@ -25,5 +24,6 @@ int	stack_double_swap(t_stack	*stack_a, t_stack *stack_b)
 		stack_swap(stack_a);
 		stack_swap(stack_b);
 	}
+	ft_printf("ss\n");
 	return (1);
 }
