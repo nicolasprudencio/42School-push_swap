580249f74a0cf5d00a0259fb45a45c92883b64cb
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-10-26 00:49:43 -0300

    check_data function added to file

--- srcs/stack/stack_define.c
+++ srcs/stack/stack_define.c
@@ -6,13 +6,33 @@
 /*   By: nicolas <nicolas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 18:39:47 by nicolas           #+#    #+#             */
-/*   Updated: 2023/10/25 20:56:13 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/10/26 00:47:02 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 // needs to treat the -44-44 example number
+
+static int	check_stack(t_dlist *stack)
+{	
+	t_dlist	*current;
+	t_dlist	*temp;
+
+	if (stack->next)
+		temp = stack->next;
+	else
+		return (1);	
+	current = stack;
+	while (temp)
+	{
+		if (current->value == temp->value)
+			return (0);
+		temp = temp->next;
+	}
+	check_stack(current->next);
+	return (1);
+}
 
 int stack_define(int argc, char **argv, t_stack *stack)
 {
@@ -33,6 +53,8 @@ int stack_define(int argc, char **argv, t_stack *stack)
 				return (0);
 		}
 		stack_addbtm(&stack, ft_dlnew(ft_atoi(argv[i])));
+		if (!check_stack(stack->top))
+			return (0);
 	}
 	return (1);
 }
