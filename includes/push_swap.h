f69b2bd8bea0be4bd943a58eddc0675312ab7f5a
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-10-26 00:51:07 -0300

    push_swap headerfile updated

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,9 +6,11 @@
 /*   By: nicolas <nicolas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 17:46:07 by nicolas           #+#    #+#             */
-/*   Updated: 2023/10/25 20:05:20 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/10/26 00:24:21 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* Ideia possível, ter uma estrutura com tamanho total da stack, e que possui referencia para o top das duas stacks */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
@@ -17,5 +19,7 @@
 # include "libft.h"
 
 void	throw_error(t_stack *stck_a, t_stack *stck_b);
+int		has_lower(t_dlist *head, int value);
+int		has_greater(t_dlist *head, int value);
 
 #endif
