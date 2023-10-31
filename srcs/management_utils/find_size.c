9a57a11defe24825b3e045bd3fc9ed308f501eca
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-10-31 08:41:36 -0300

    first commit

---
+++ srcs/management_utils/find_size.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   find_size.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 19:29:47 by nprudenc          #+#    #+#             */
+/*   Updated: 2023/10/31 01:20:57 by nprudenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	find_stack_size(t_dlist *lst)
+{	
+	int	lst_len;
+	t_dlist	*temp;
+
+	lst_len = 0;
+	temp = lst;
+	while (temp)
+	{
+		temp = temp->next;
+		lst_len++;
+	}
+	return (lst_len);
+}
