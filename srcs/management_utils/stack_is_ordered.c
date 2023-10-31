f3e885494d4897e1266e2c85a2cb94709c107ce5
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-10-31 08:42:05 -0300

    stack is ordered

---
+++ srcs/management_utils/stack_is_ordered.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_is_ordered.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 19:02:01 by nprudenc          #+#    #+#             */
+/*   Updated: 2023/10/31 02:13:04 by nprudenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	stack_is_ordered(t_dlist *stack)
+{
+	t_dlist	*temp;
+
+	temp = stack;
+	while (temp && temp->next)
+	{
+		if (temp->value > temp->next->value)
+			return (0);
+		temp = temp->next;
+	}
+	return (1);
+}
